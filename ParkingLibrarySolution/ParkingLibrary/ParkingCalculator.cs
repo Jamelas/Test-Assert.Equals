a573a802fdeabb54c22aa8449307e9c0cdcd6167
Jamie <jamie.styx@gmail.com>
2023-05-12 16:19:23 +0900

    Class to be tested created

---
+++ ParkingLibrarySolution/ParkingLibrary/ParkingCalculator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingLibrary
+{
+    public class ParkingCalculator
+    {
+        private const double StaffCharge = 30;
+        private const double StudentDiscount = 0.25;
+        private const double GeneralCharge = 5;
+
+        public double CalculateParkingCharge(PersonType myPersonType, int hours)
+        {
+            if (myPersonType == PersonType.Staff)
+            {
+                return StaffCharge;
+            }
+            else if (myPersonType == PersonType.General)
+            {
+                return hours * GeneralCharge;
+            }
+            else if (myPersonType == PersonType.Student)
+            {
+                return hours * GeneralCharge * (1 - StudentDiscount);
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
