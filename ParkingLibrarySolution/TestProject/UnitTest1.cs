3289241042b5c8dad6442be24e96ded1220cf390
Jamie <jamie.styx@gmail.com>
2023-05-12 16:40:50 +0900

    Added tests for class method

--- ParkingLibrarySolution/TestProject/UnitTest1.cs
+++ ParkingLibrarySolution/TestProject/UnitTest1.cs
@@ -1,11 +1,46 @@
+using ParkingLibrary;
+
 namespace TestProject
 {
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+        private ParkingCalculator _objParking;
+        private PersonType _objPerson;
+
+        [TestInitialize]
+        public void StartMyTests()
         {
+            _objParking = new ParkingCalculator();
+            _objPerson = new PersonType();
+        }
+
+        [TestMethod]
+        public void TestMethodGeneral()
+        {
+            _objPerson = PersonType.General;
+            Assert.AreEqual(10.0, _objParking.CalculateParkingCharge(_objPerson, 2));
+        }
+
+        [TestMethod]
+        public void TestMethodStaff()
+        {
+            _objPerson = PersonType.Staff;
+            Assert.AreEqual(30.0, _objParking.CalculateParkingCharge(_objPerson, 2));
+        }
+
+        [TestMethod]
+        public void TestMethodStudent()
+        {
+            _objPerson = PersonType.Student;
+            Assert.AreEqual(7.5, _objParking.CalculateParkingCharge(_objPerson, 2));
+        }
+
+        [TestMethod]
+        public void TestMethodNone()
+        {
+            _objPerson = PersonType.None;
+            Assert.AreEqual(0, _objParking.CalculateParkingCharge(_objPerson, 2));
         }
     }
 }
